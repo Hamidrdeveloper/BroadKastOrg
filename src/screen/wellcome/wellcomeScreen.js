410cdff0312d3ca851858afdb1eccc8143423131
Your Name <you@example.com>
2021-11-04 10:28:17 +0330

    Feature/Permission

--- src/screen/wellcome/wellcomeScreen.js
+++ src/screen/wellcome/wellcomeScreen.js
@@ -165,9 +165,25 @@ export const WellcomeScreen = ({navigation}) => {
       console.log('dataUser', e);
     }
   };
-
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      this.setState({ userInfo });
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
   useEffect(() => {
-    GoogleSignin.signIn();
+    signIn();
     ApiCalendar.handleAuthClick();
     console.log('ApiCalendar.sign', ApiCalendar.sign);
 
