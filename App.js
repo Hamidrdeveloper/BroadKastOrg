3744967ec197539d52929279f74dd13ff2caa6d6
Your Name <you@example.com>
2021-11-06 02:26:30 +0330

    Feature/Permission

--- App.js
+++ App.js
@@ -93,8 +93,24 @@ export default class App extends React.Component {
       ApiCalendar.handleAuthClick();
       console.log('logged in');
     } else if (name === 'sign-out') {
-      ApiCalendar.handleSignoutClick();
-      console.log('logged out');
+      try {
+        console.log("Start google")
+         GoogleSignin.hasPlayServices();
+        const userInfo =  GoogleSignin.signIn();
+        this.setState({ userInfo });
+      } catch (error) {
+        console.log(error+"");
+  
+        if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+          // user cancelled the login flow
+        } else if (error.code === statusCodes.IN_PROGRESS) {
+          // operation (e.g. sign in) is in progress already
+        } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+          // play services not available or outdated
+        } else {
+          // some other error happened
+        }
+      }
     }
   }
 
